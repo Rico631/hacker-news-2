801374d62ad2557e23f2ef19460f302e19710eec
Dmitrii <rico@mail.ru>
2024-08-31 18:52:35 +0300

    лекция 4

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { NewsItem } from "./NewsItem/NewsItem";
+
+const initNews = [
+  {
+    title: "Первая новость",
+    url: "example.com",
+    username: "Первый пользователь",
+    date: "10.10.24",
+    score: 10,
+  },
+  {
+    title: "Вторая новость",
+    url: "example.com",
+    username: "Второй пользователь",
+    date: "16.10.24",
+    score: 1243,
+  },
+  {
+    title: "Третья новость",
+    url: "example.com",
+    username: "Третий пользователь",
+    date: "14.10.24",
+    score: 100,
+  },
+];
+
+const new_news = {
+  title: "N новость",
+  url: "example.com",
+  username: "N пользователь",
+  date: "10.10.24",
+  score: 10,
+};
 
 function App() {
+  const checkStorage = () =>
+    JSON.parse(window.localStorage.getItem("newsKey")) || initNews;
+  const [news, setNews] = useState(checkStorage);
+  // const [count, setCount] = useState(0);
+
+  // console.log("render App");
+
+  useEffect(() => {
+    // console.log("effect news");
+    window.localStorage.setItem("newsKey", JSON.stringify(news));
+  }, [news]);
+
+  const setNewsHandler = () => {
+    setNews((prev) => [...prev, new_news]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <button onClick={() => setCount(count + 1)}>{count}</button> */}
+      <div>Количество новостей: {news.length}</div>
+      <button onClick={setNewsHandler}>Добавить новость</button>
+      {news.map((item, i) => {
+        return (
+          <NewsItem
+            key={i}
+            title={item.title}
+            url={item.url}
+            username={item.username}
+            date={item.date}
+            score={item.score}
+          />
+        );
+      })}
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { NewsItem } from "./NewsItem/NewsItem";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <>
     <App />
-  </React.StrictMode>
+    {/* <NewsItem
+      title="Первая новость"
+      url="example.com"
+      username="Пользователь 1"
+      date="10.10.24"
+      score={10}
+    />
+    <NewsItem
+      title="Вторая новость"
+      url="example.com"
+      username="Пользователь 2"
+      date="11.10.24"
+      score={2}
+    /> */}
+  </>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// function App() {
+//   const [state, setState] = useState(false)
+//   return (
+//     <div>
+//       <p>Текущее состояние: {String(state)}</p>
+//       <button onClick={() => setState(!state)}>Переключить</button>
+//     </div>
+//   )
+// }
